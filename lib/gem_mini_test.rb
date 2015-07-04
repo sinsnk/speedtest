ca172c485f1c0d39ca8b32db63f12e4a1172a9c2
sinsnk <cb.so.devope@gmail.com>
2015-07-04 16:50:06 +0900

    first commit

---
+++ lib/gem_mini_test.rb
@@ -0,0 +1,5 @@
+require "gem_mini_test/version"
+
+module GemMiniTest
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,4 @@
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'gem_mini_test'
+
+require 'minitest/autorun'
